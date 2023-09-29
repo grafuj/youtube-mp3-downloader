c0cd57e627706d8a1cebbab603d0b5c3a9396e4d
Graham Fujisawa <sfrrki@gmail.com>
2023-09-29 13:16:45 -0700

    add form and css reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Youtube to MP3 Converter</h1>
+      <p className="content_description">Pull an MP3 off a youtube video</p>
+      <form className='form'>
+        <input className='form_input' type='text' value={url} placeholder='Paste a Youtube URL'></input>
+        <button type='submit' className='form_button'>Convert</button>
+      </form>
+
+      <a href="" className="download_link">Download MP3</a>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
