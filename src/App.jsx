c3c6e048ddb5c329d74f396153d5b523ac672f27
Graham Fujisawa <sfrrki@gmail.com>
2023-10-01 15:49:49 -0700

    add css to main page

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ function App() {
 
   // input needs value
   return (
-    <>
-      <h1 className='page_title'>Youtube to MP3 Converter</h1>
-      <p className="content_description">Pull an MP3 off a youtube video</p>
+    <div className='content'>
+      <h1 className='page_title light_blue'>Youtube to MP3 Converter</h1>
+      <p className="content_description light_blue">Pull an MP3 off a youtube video</p>
       <form className='form'>
         <input className='form_input' type='text' placeholder='Paste a Youtube URL'></input>
         <button type='submit' className='form_button'>Convert</button>
       </form>
 
-      <a href="" className="download_link">Download MP3</a>
-    </>
+      <a href="" className="download_link light_blue">Download MP3</a>
+    </div>
   );
 }
 
