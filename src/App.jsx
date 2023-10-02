3981d037ed713612bc0c122c077048693448b4c9
Graham Fujisawa <sfrrki@gmail.com>
2023-10-01 18:03:02 -0700

    update styles, add mobile styles, start axios and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const inputUrlRef = useRef();
+  const [urlResult, setUrlResult] = useState(null);
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(inputUrlRef.input.value)
+
+    const options = {
+      method: 'get',
+      url: 'https://youtube-mp36.p.rapidapi.com/dl',
+      params: {id: 'UxxajLWwzqY'},
+      headers: {
+        'X-RapidAPI-Key': 'ffbf0b6fdamsh95902cd4574e50fp1ae931jsn794044a62ced',
+        'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com'
+      }
+    }
+    axios(options)
+      .then(res => setUrlResult(res.data))
+      .catch(error => console.error("Error:", error))
+  }
 
   // input needs value
   return (
     <div className='content'>
       <h1 className='page_title light_blue'>Youtube to MP3 Converter</h1>
       <p className="content_description light_blue">Pull an MP3 off a youtube video</p>
-      <form className='form'>
-        <input className='form_input' type='text' placeholder='Paste a Youtube URL'></input>
-        <button type='submit' className='form_button'>Convert</button>
+      <form className='form' onSubmit={handleSubmit}>
+        <input ref={inputUrlRef} className='form_input light_blue' type='text' placeholder='Paste a Youtube URL...'></input>
+        <button type='submit' className='form_button light_blue'>Convert</button>
       </form>
 
       <a href="" className="download_link light_blue">Download MP3</a>
