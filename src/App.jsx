edb7d1873686834ce61175bb65ffc3e7b7e01a75
Graham Fujisawa <sfrrki@gmail.com>
2023-10-18 16:49:50 -0700

    add more consise name for api handle

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       method: 'get',
       url: 'https://youtube-mp36.p.rapidapi.com/dl',
       headers: {
-        'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+        'X-RapidAPI-Key': import.meta.env.RAPID_API_KEY,
         'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com'
       },
       params: { id: youtubeID },
